b8498b5aa7bb54491e3e61cb65394b5250af36db
TAMIM <tamimiqbal33123@gmail.com>
2026-01-09 02:44:04 +0600

    fix small issue

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-eda88673'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.udk8pjmroq8"
+    "revision": "0.sk2lm68qj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
