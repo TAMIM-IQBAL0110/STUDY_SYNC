b16121d80221a26690f510899b1e656b3e9b3852
TAMIM <tamimiqbal33123@gmail.com>
2026-01-08 22:27:11 +0600

    Update: AccountSetting shadows and colors, CategoryDisplay backend integration with fallback

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-eda88673'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lpr5peselh"
+    "revision": "0.u62set6kfs8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
